0ee9bf3e1a6b3213f0fb4a314c68073c4163c9fe
devalenya1 <127754942+devalenya1@users.noreply.github.com>
2023-08-03 21:58:12 +0100

    Update my_webview.dart

--- lib/ui/screens/my_webview.dart
+++ lib/ui/screens/my_webview.dart
@@ -1,29 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class MyWebView extends StatelessWidget {
+// class MyWebView extends StatelessWidget {
   // final String title;
   // final String selectedUrl;
-late final WebViewController controller;
+// late final WebViewController controller;
  // final Completer<WebViewController> _controller = Completer<WebViewController>();
+class MyWebView extends StatefulWidget {
+  const MyWebView({super.key});
 
-  // MyWebView({
-  //   @required this.title,
-  //   @required this.selectedUrl,
-  // });
+  @override
+  State<MyWebView> createState() => _MyWebViewState();
+}
+class _MyWebViewState extends State<MyWebView> {
+  late final WebViewController controller;
+ 
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse('http://kayanschool.atwebpages.com/chat'),
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Chat Room"),
+        title: const Text('Chat Room'),
       ),
-      body: WebView(
-        initialUrl: "http://kayanschool.atwebpages.com/chat",
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (WebViewController webViewController) {
-          _controller.complete(webViewController);
-        },
-      ));
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
   }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text("Chat Room"),
+  //     ),
+  //     body: WebView(
+  //       initialUrl: "http://kayanschool.atwebpages.com/chat",
+  //       javascriptMode: JavascriptMode.unrestricted,
+  //       onWebViewCreated: (WebViewController webViewController) {
+  //         _controller.complete(webViewController);
+  //       },
+  //     ));
+  // }
 }
