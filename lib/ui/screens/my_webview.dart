cd4006aac6917c5cb4eb6375b1d3f8461651c3ca
devalenya1 <127754942+devalenya1@users.noreply.github.com>
2023-08-03 21:28:18 +0100

    Update my_webview.dart

--- lib/ui/screens/my_webview.dart
+++ lib/ui/screens/my_webview.dart
@@ -2,24 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class MyWebView extends StatelessWidget {
-  final String title;
-  final String selectedUrl;
+  // final String title;
+  // final String selectedUrl;
+late final WebViewController controller;
+ // final Completer<WebViewController> _controller = Completer<WebViewController>();
 
-  final Completer<WebViewController> _controller = Completer<WebViewController>();
-
-  MyWebView({
-    @required this.title,
-    @required this.selectedUrl,
-  });
+  // MyWebView({
+  //   @required this.title,
+  //   @required this.selectedUrl,
+  // });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text("Chat Room"),
       ),
       body: WebView(
-        initialUrl: selectedUrl,
+        initialUrl: "http://kayanschool.atwebpages.com/chat",
         javascriptMode: JavascriptMode.unrestricted,
         onWebViewCreated: (WebViewController webViewController) {
           _controller.complete(webViewController);
