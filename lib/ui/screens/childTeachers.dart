25bc75c14958111b89011b99a5eb164d6f9fa401
devalenya1 <127754942+devalenya1@users.noreply.github.com>
2023-08-22 02:46:43 -0700

    Update childTeachers.dart

--- lib/ui/screens/childTeachers.dart
+++ lib/ui/screens/childTeachers.dart
@@ -19,6 +19,7 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:eschool/utils/constants.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:async';
 
 class ChildTeachersScreen extends StatefulWidget {
   final int childId;
